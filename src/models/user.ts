4dcc39b29bdbee3d9c52ef5f686fd027f0d28014
jekovniki <jekovniki95@gmail.com>
2023-11-25 10:52:08 +0200

    Update user schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,13 +1,31 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IUserModel extends Document {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
+    gender: "MALE" | "FEMALE" | "OTHER";
+    createdAt: Date;
+    updatedAt: Date;
+    dateOfBirth: Date;
+    lastLogin: Date;
+    userDevice: string;
+    firstName: string;
+    lastName: string;
+    companyId: string;
 }
 
 const userSchema: Schema = new Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    gender: {type: String, enum: ['MALE', 'FEMALE', 'OTHER'], default: 'MALE'},
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+    dateOfBirth: {type: Date, default: Date.now },
+    lastLogin: { type: Date, default: null },
+    userDevice: { type: String, default: null },
+    firstName: { type: String, default: null },
+    lastName: { type: String, default: null },
+    companyId: { type: String, default: null },
 });
 
 export default mongoose.model<IUserModel>('User', userSchema);
