33fdc91fe8c4702dd569f3214422100a976eed0c
jekovniki <jekovniki95@gmail.com>
2024-02-19 22:42:40 +0200

    Remove the cache

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { logger } from "./utils/logger";
 import { server } from "./libraries/server";
 import { database } from "./libraries/database";
-import { cache } from "./libraries/cache";
 
 (async function() {
     try {
         server.start();
         database.connect();
-        await cache.clearAll();
     } catch (error) {
         logger.error(error);
     }
